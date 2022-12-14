b713ec64d60362735eeae1c68a38e91ea8d78fea
Daniel Aguilera <dan.aguilera97@gmail.com>
2022-12-13 18:46:38 -0600

    linked all the files within app.js and began creating handlebars.js fileslinked bootstrap to add CSS

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(express.urlencoded({ extended: true })); // New
+
+app.use(express.json()); // New
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+const handlebars = exphbs.create({ extname: '.hbs', });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+app.get('', (req,res) => {
+    res.render('home')
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
